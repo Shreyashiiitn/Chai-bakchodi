4a196a783d894ed4005fd0fdbb7836b1fa1ea6bd
Shreyashiiitn <118331370+Shreyashiiitn@users.noreply.github.com>
2024-04-15 21:50:33 +0530

    Configuration of multer and other things is done

---
+++ src/Middlewares/multer.middleware.js
@@ -0,0 +1,13 @@
+import multer from "multer";
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, "./public/temp")
+    },
+    filename: function (req, file, cb) {
+       // This can also be removed
+      cb(null, file.originalname)  // file.orginaname
+    }
+  })
+  
+export  const upload = multer({ storage: storage })
